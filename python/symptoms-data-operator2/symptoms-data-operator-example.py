19141ab42d522845f64eb219fb71aafbb370dbc7
up201900050 <up201900050@fe.up.pt>
2021-04-29 12:17:23 +0100

    update operators

--- python/symptoms-data-operator2/symptoms-data-operator-example.py
+++ python/symptoms-data-operator2/symptoms-data-operator-example.py
@@ -2,7 +2,7 @@
 # pip install pandas
 import pandas as pd
 import numpy as np
-import symptoms-data-operator as sdo
+import gender_data_operator as gdo
  
 complete_path = '/Users/rogeriocarmine/Estudo/MESW/trudata-desenv/trudata/trudata/git-operators/data-cleaning-operators/python/gender-data-operator1/'
 
@@ -11,6 +11,6 @@ path_to_save_csv = complete_path+'trudata-outbreak-data-fixed.csv'
  
 df = pd.read_csv(path_to_read_csv)
  
-sdo.symptoms_validate(df, 'symptoms-fever')
+gdo.gender_transform(df, 'gender')
  
 df.to_csv(path_to_save_csv)
--- python/symptoms-data-operator2/symptoms-data-operator.py
+++ python/symptoms-data-operator2/symptoms-data-operator.py
@@ -3,10 +3,9 @@
 import pandas as pd
 import numpy as np
  
-def symptoms_validate(data_frame, column_to_transform):
+def symptoms_transform(data_frame, column_to_transform):
 
     yes = ['SIM', 'YES', 'Y', 'X', 'SI']
     no = ['NAO', 'NO', 'N']
-    
     data_frame[column_to_transform] = np.where(data_frame[column_to_transform].isin(yes), 1, data_frame[column_to_transform])
     data_frame[column_to_transform] = np.where(data_frame[column_to_transform].isin(no), 0, data_frame[column_to_transform])
