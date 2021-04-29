89ba5177093396cc9c4b9b30e98ecd426a2a33ff
up201900050 <up201900050@fe.up.pt>
2021-04-29 01:31:32 +0100

    change file names

---
+++ python/gender-data-operator2/gender-data-operator-02.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+ 
+def gender_transform(data_frame, column_to_transform):
+    male_synonym = ['M', 'Masc', 'Homem', 'Man', 'Male', 'M.', 'Masc.']
+    female_synonym = ['F', 'Fem', 'Mulher', 'Woman', 'Female', 'Fem.', 'Wom.', 'Femenino']
+    data_frame[column_to_transform] = np.where(data_frame[column_to_transform].isin(male_synonym), 'M', data_frame[column_to_transform])
+    data_frame[column_to_transform] = np.where(data_frame[column_to_transform].isin(female_synonym), 'F', data_frame[column_to_transform])
